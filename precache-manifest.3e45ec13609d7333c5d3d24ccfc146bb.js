c7f2513aefaad61cc4257641da7b51e8b64f45ad
kimyoungjun <youngs3580@naver.com>
2020-08-23 15:04:51 +0900

    Updates

---
+++ precache-manifest.3e45ec13609d7333c5d3d24ccfc146bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3a2bf63672ca6953cf94b56fe7fc10",
+    "url": "/lunch/index.html"
+  },
+  {
+    "revision": "8e9eae94257f5d338afc",
+    "url": "/lunch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6241fdc7def3cd207098",
+    "url": "/lunch/static/js/2.2527bbb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lunch/static/js/2.2527bbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9eae94257f5d338afc",
+    "url": "/lunch/static/js/main.b209ff00.chunk.js"
+  },
+  {
+    "revision": "8143701af80a7288dc5b",
+    "url": "/lunch/static/js/runtime-main.afa09495.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lunch/static/media/logo.5d5d9eef.svg"
+  }
+]);
