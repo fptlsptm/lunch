d7bd698d7f1ae762596d71593e379407e8f2b339
kimyoungjun <youngs3580@naver.com>
2020-08-23 15:16:17 +0900

    too commit

---
+++ build/precache-manifest.d4ec7025a5e29c2c2d98e41d8f99f0bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25694032379c9746383507a2f9c95d6",
+    "url": "/lunch/index.html"
+  },
+  {
+    "revision": "034ad94078f84c00fe36",
+    "url": "/lunch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6241fdc7def3cd207098",
+    "url": "/lunch/static/js/2.2527bbb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lunch/static/js/2.2527bbb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034ad94078f84c00fe36",
+    "url": "/lunch/static/js/main.02078396.chunk.js"
+  },
+  {
+    "revision": "8143701af80a7288dc5b",
+    "url": "/lunch/static/js/runtime-main.afa09495.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lunch/static/media/logo.5d5d9eef.svg"
+  }
+]);
